fdc06a6965ca9d28788487056dc96618829ad0af
srmora-ec <alvarezjoseandres@outlook.es>
2025-09-12 17:30:07 -0500

    Prueba

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="spinner" style={{ width: "40px", height: "40px" }}></div>
+    </div>
+  )
+}
