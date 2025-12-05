76e077f5243e233227a0f44687f079da52163975
srmora-ec <alvarezjoseandres@outlook.es>
2025-12-04 23:22:29 -0500

    Prueba de arregle de bug x14

--- app/evaluacion/[idmodelo]/page.tsx
+++ app/evaluacion/[idmodelo]/page.tsx
@@ -21,7 +21,7 @@ import {
 import { UploadOutlined, FolderOpenOutlined } from "@ant-design/icons"
 import { Modelo, type ModeloData, type Nodo } from "@/types/modelo"
 import ModeloSvgViewer from "@/components/modelo-svg-viewer"
-import UnidimensionalSensitivityAnalysis from "@/ComAnalisisDeSensibilidad/unidimensionalsensitivityanalysis"
+import UnidimensionalSensitivityAnalysis from "@/components/unidimensionalsensitivityanalysis"
 import UnidimensionalSensitivityMAUT from "@/ComAnalisisDeSensibilidad/UnidimensionalSensitivityMAUT"
 import Modal from "@/components/Modal"
 import { supabase } from "@/lib/supabase"
