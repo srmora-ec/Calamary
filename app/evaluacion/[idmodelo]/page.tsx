49d97be6e29f7a97eef284e7a4fc5c870470db03
srmora-ec <alvarezjoseandres@outlook.es>
2025-12-04 23:32:09 -0500

    Prueba de arregle de bug x16

--- app/evaluacion/[idmodelo]/page.tsx
+++ app/evaluacion/[idmodelo]/page.tsx
@@ -21,7 +21,7 @@ import {
 import { UploadOutlined, FolderOpenOutlined } from "@ant-design/icons"
 import { Modelo, type ModeloData, type Nodo } from "@/types/modelo"
 import ModeloSvgViewer from "@/components/modelo-svg-viewer"
-import UnidimensionalSensitivityAnalysis from "@/app/evaluacion/unidimensionalsensitivityanalysis"
+import UnidimensionalSensitivityAnalysis from "@/app/evaluacion/[idmodelo]/unidimensionalsensitivityanalysis"
 import UnidimensionalSensitivityMAUT from "@/ComAnalisisDeSensibilidad/UnidimensionalSensitivityMAUT"
 import Modal from "@/components/Modal"
 import { supabase } from "@/lib/supabase"
--- app/evaluacion/[idmodelo]/unidimensionalsensitivityanalysis.tsx
+++ app/evaluacion/[idmodelo]/unidimensionalsensitivityanalysis.tsx
@@ -4,8 +4,8 @@ import { useState } from "react"
 import { Select, Button, message, Spin, Card } from "antd"
 import type { Nodo } from "@/types/modelo"
 import { useTranslation } from "react-i18next"
-import { useNotification } from "../../components/NotificationProvider"
-import Header from "../../components/Header"
+import { useNotification } from "../../../components/NotificationProvider"
+import Header from "../../../components/Header"
 // import LanguageSwitcher from "@/components/LanguageSwitcher";
 // import "@/i18n";
 
